ab80a112e5c23678953be51f844d8e49de10f8c0
sakshiandhale12 <sakshiandhale12@gmail.com>
2023-09-21 18:48:36 +0530

    initial commit

--- shoppsy/settings.py
+++ shoppsy/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'shoppsy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shoppsy',
+        'USER': 'root',
+        'PASSWORD': 'epps@123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
