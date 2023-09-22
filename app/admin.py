db65869e8fa4b4b5b7bb970b3cc813797505d81e
sakshiandhale12 <sakshiandhale12@gmail.com>
2023-09-22 19:07:41 +0530

    initial commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from.models import (
+    Custemer,
+    Product,
+    Cart,
+    OrderPlaced
+)
 
 # Register your models here.
+@admin.register(Custemer)
+class CustemerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','locality','city','zipcode','state']
+    
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id','title','selling_price','discount_price','brand','category','Product_image']
+    
+    
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display=['user', 'product', 'quantity']
+    
+    
+@admin.register(OrderPlaced)   
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'customer', 'Product', 'quantity', 'Ordered_date', 'status')
+
+    
--- app/models.py
+++ app/models.py
@@ -1,3 +1,103 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+STATE_CHOICES=(
+    ('Andaman & Nicobar Island','Andaman & Nicobar Island'),
+    ('Andhra Pradesh','andhrapradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Goa','Goa'),
+    ('Gujarat	','Gujarat	'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Madhya Pradesh	','Madhya Pradesh	'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya	','Meghalaya	'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu	','Tamil Nadu	'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttar Pradesh	','Uttar Pradesh	'),
+    ('Uttarakhand','Uttarakhand'),
+    ('West Bengal	','West Bengal	')
+)
+class Custemer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=200)
+    locality= models.CharField(max_length=200)
+    city= models.CharField(max_length=50)
+    zipcode= models.IntegerField()
+    state= models.CharField( choices=STATE_CHOICES, max_length=50)
+    
+    def __str__(self):
+        return str(self.id)
+    
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
+    ('BW', 'Bottom Wear')
+)
+class Product(models.Model):
+    title= models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discount_price=models.FloatField()
+    brand=models.CharField(max_length=100)
+    category=models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    Product_image=models.ImageField(upload_to='productimg')
+    
+    def __str__(self):
+        return str(self.id)
+    
+# class Cart(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     product=models.ForeignKey(User,on_delete=models.CASCADE)
+#     quantity=models.PositiveIntegerField(default=1)
+    
+#     def __str__(self):
+#         return str(self.id)
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='carts')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='carts_for_product')
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
+    
+STATE_CHOICES=(
+    ('Accepted','Accepted'),
+    ('packed','packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel')
+)
+
+# class OrderPlaced(models.Model):
+#     user= models.CharField(max_length=100)
+#     # Custemer=models.ForeignKey(User, on_delete=models.CASCADE)
+#     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+#     Product=models.ForeignKey(User, on_delete=models.CASCADE)
+#     quanity=models.PositiveBigIntegerField(default=1)
+#     Ordered_date=models.DateTimeField(auto_now_add=True)
+#     status=models.CharField(max_length=50, status=STATE_CHOICES, default='pending')
+    
+class OrderPlaced(models.Model):
+    user = models.CharField(max_length=100)  # This field needs a CharField or ForeignKey, not just CharField
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)  # Assuming Product is another model
+    quantity = models.PositiveBigIntegerField(default=1)
+    Ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATE_CHOICES, default='pending')
